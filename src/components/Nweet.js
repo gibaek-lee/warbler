99cfc71d920acdafb216404154653559ff1224ad
gibaek-lee <bbsnake10@gmail.com>
2020-10-20 00:38:47 +0900

    feat and refactor: Nweet 컴포넌트 delete 메서드 추가 & firestore api 연동

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,12 +1,20 @@
+import { dbService } from "fbase"
 import React from "react"
 
 const Nweet = ({nweetObj, isOwner}) => {
+  const onDeleteCilck = async () => {
+    const ok = window.confirm("Are you sure you want to delete this nweet?")
+    if (ok) {
+      await dbService.doc(`nweets/${nweetObj.id}`).delete()
+    }
+  }
+
   return (
     <div>
       <h4>{nweetObj.text}</h4>
       {isOwner && (
         <>
-          <button>Delete Nweet</button>
+          <button onClick={onDeleteCilck}>Delete Nweet</button>
           <button>Edit Nweet</button>
         </>
       )}
