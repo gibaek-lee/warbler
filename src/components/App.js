99b797cf625e0fc876a041eb3b69103700eb64e3
gibaek-lee <bbsnake10@gmail.com>
2020-10-22 02:19:20 +0900

    refactor and fix: 불필요한 state isLoggedIn을 userObj로 대체 & useEffect 내부 비동기 처리 컴포넌트 unmount 후 불필요한 state set 방지 처리

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,24 @@ import { authService } from 'fbase'
 
 function App() {
   const [init, setInit] = useState(false)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
   const [userObj, setUserObj] = useState(null)
 
   useEffect(() => {
-    authService.onAuthStateChanged(user => {
-      if (user) {
-        setIsLoggedIn(true)
-        setUserObj(user)
-      } else {
-        setIsLoggedIn(false)
-      }
+    let isMounted = true
 
-      setInit(true)
+    authService.onAuthStateChanged(user => {
+      if(isMounted) {
+        setUserObj(user || null)
+        setInit(true)  
+      }
     })
+
+    return () => isMounted = false
   }, [])
 
   return (
     <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : "Initializing...."}
+      {init ? <AppRouter userObj={userObj} /> : "Initializing...."}
       <footer>&copy Nwitter {new Date().getFullYear()}</footer>
     </>
   )
