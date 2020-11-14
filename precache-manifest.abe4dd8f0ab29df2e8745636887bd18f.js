364d0e327daf05dc3dba4580f731416dfdc99e14
gibaek-lee <bbsnake10@gmail.com>
2020-11-14 09:55:51 +0900

    Updates

---
+++ precache-manifest.abe4dd8f0ab29df2e8745636887bd18f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91e0987a96ac39b1027729e1530ae2ad",
+    "url": "/warbler/index.html"
+  },
+  {
+    "revision": "62c1cd9054b3267e8409",
+    "url": "/warbler/static/css/main.1d44a81b.chunk.css"
+  },
+  {
+    "revision": "aef86d298e51fbcd4412",
+    "url": "/warbler/static/js/2.b4a606be.chunk.js"
+  },
+  {
+    "revision": "8e844923efaee6906e00c8fde9e55716",
+    "url": "/warbler/static/js/2.b4a606be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62c1cd9054b3267e8409",
+    "url": "/warbler/static/js/main.cd24bdfa.chunk.js"
+  },
+  {
+    "revision": "18d87809ea32cfbb2b70",
+    "url": "/warbler/static/js/runtime-main.3259b50f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/warble/precache-manifest.b50eecc4ddce190419f9e6be3b3cf883.js"
+  "/warbler/precache-manifest.abe4dd8f0ab29df2e8745636887bd18f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/warble/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/warbler/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
